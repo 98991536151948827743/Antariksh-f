2429f4b86a577d106ae2533349677545ea2e7579
98991536151948827743 <Rahul5g3d.official@gmail.com>
2025-10-17 23:37:59 +0530

    fix vercel analytics + seo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import 
+import { inject } from '@vercel/analytics';
+import { HelmetProvider } from 'react-helmet-async';
+
+// components
 import FloatingButtons from './components/layout/top';
 import ScrollToTop from './pages/extraPages/ScrollToTop';
+import TechspardhaPopup from './components/popUP/techspardha.pupup';
 
+// pages
 import Home1 from './pages/homePage';
 import Contact from './pages/contactPage';
 import Gallery from './pages/galleryPage';
@@ -11,84 +16,75 @@ import EventPage from './pages/eventPage';
 import LayoutWrapper from './pages/LayoutWrapper';
 import NotFoundPage from './pages/NotFoundPage';
 import PdfUpload from './pages/Upload_pdf/PdfUpload';
+import BuiltByPage from './pages/extraPages/builtBy';
+import LaunchpadPage from '../src/pages/launchpad/launchPadPage';
 
-// Teams
+// teams
 import KalpaPage from './pages/teams/kalpa.page';
 import ObservatoryPage from './pages/teams/observatory.page';
 import NewsletterPage from './pages/teams/newsletter.page';
-import DesignMediaPage  from './pages/teams/media.page';
+import DesignMediaPage from './pages/teams/media.page';
 import WebTechPage from './pages/teams/tech.page';
 import CoreManagementPage from './pages/teams/management.page';
 import ManagementPage from './pages/teams/management.page';
 import KhagolQuizzingPage from './pages/teams/khagol.page';
 import DiscussionPage from './pages/teams/discussion.page';
 
-
-//blog
+// blogs
 import BlogPage from './pages/Blog/blogPage';
 import BlogDetailsPage from './pages/Blog/blogDetailsPage';
 import AstroFactsPage from './pages/Blog/astroFacts';
 import SpaceExplorationsPage from './pages/Blog/SpaceExplorationsPage';
 
-//contact
-import ContactPage from './pages/api/contact'
-import BuiltByPage from './pages/extraPages/builtBy';
-import TechspardhaPopup from './components/popUP/techspardha.pupup';
+// ✅ Initialize Vercel Analytics once here (outside JSX)
+inject();
 
-//Launchpad
-import LaunchpadPage from '../src/pages/launchpad/launchPadPage';
-// App Component with Routing
 function App() {
   return (
     <BrowserRouter>
-            <ScrollToTop />
-          <FloatingButtons />
-          <TechspardhaPopup />
-      <Routes>
-        {/* Homepage without layout wrapper */}
-        <Route path="/" element={<Home1 />} />
+      <HelmetProvider>
+        <ScrollToTop />
+        <FloatingButtons />
+        <TechspardhaPopup />
 
-        {/* Teams pages (no background if you want) */}
+        <Routes>
+          {/* Homepage */}
+          <Route path="/" element={<Home1 />} />
 
+          {/* Layout wrapper for inner pages */}
+          <Route element={<LayoutWrapper />}>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/events" element={<EventPage />} />
 
-        {/* Pages with global layout wrapper */}
-        <Route element={<LayoutWrapper />}>
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/events" element={<EventPage />} />
-          <Route path="/teams/observatory" element={<ObservatoryPage />} />
-          <Route path="/teams/kalpa" element={<KalpaPage />} />
+            {/* Teams */}
+            <Route path="/teams/observatory" element={<ObservatoryPage />} />
+            <Route path="/teams/kalpa" element={<KalpaPage />} />
+            <Route path="/teams/newsletter" element={<NewsletterPage />} />
+            <Route path="/teams/design-media" element={<DesignMediaPage />} />
+            <Route path="/teams/web-tech" element={<WebTechPage />} />
+            <Route path="/teams/core-management" element={<CoreManagementPage />} />
+            <Route path="/teams/management" element={<ManagementPage />} />
+            <Route path="/teams/khagol" element={<KhagolQuizzingPage />} />
+            <Route path="/teams/discussion" element={<DiscussionPage />} />
 
-        <Route path="/teams/newsletter" element={<NewsletterPage />} />
-        <Route path="/teams/design-media" element={<DesignMediaPage />} />
-        <Route path="/teams/web-tech" element={<WebTechPage />} />
-        <Route path="/teams/core-management" element={<CoreManagementPage />} />
-        <Route path="/teams/managxement" element={<ManagementPage />} />
-        <Route path="/teams/khagol" element={<KhagolQuizzingPage />} />
-        <Route path="/teams/discussion" element={<DiscussionPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+            {/* Blogs */}
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/:id" element={<BlogDetailsPage />} />
+            <Route path="/blog/facts" element={<AstroFactsPage />} />
+            <Route path="/blog/space" element={<SpaceExplorationsPage />} />
 
+            {/* Other */}
+            <Route path="/upload-pdf" element={<PdfUpload />} />
+            <Route path="/built-by" element={<BuiltByPage />} />
+            <Route path="/launchpad" element={<LaunchpadPage />} />
 
-
-        {/* Blog Pages */}
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:id" element={<BlogDetailsPage />} />
-        <Route path="/blog/facts" element={<AstroFactsPage />} />
-        <Route path="/blog/space" element={<SpaceExplorationsPage />} />
-
-        {/* PDF Upload */}
-        <Route path="/upload-pdf" element={<PdfUpload />} />
-
-        {/* contact */}
-        <Route path='built-by' element={<BuiltByPage/>}/>
-
-
-        {/* {LaunchpadPage} */}
-        <Route path="/launchpad" element={<LaunchpadPage />} />
-        </Route>
-        
-      </Routes>
+            {/* 404 */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </HelmetProvider>
     </BrowserRouter>
   );
 }
